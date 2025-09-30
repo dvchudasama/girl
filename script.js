4827e7ef6978f934738ba3919f02788150ca4051
dvchudasama <hitidarshan@gmail.com>
2025-09-30 15:01:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const messages = [
     "Every day I spend with you is a new favorite day. You make everything better.",
     "Thanks for being my girl :)",
     "Can't wait to do life w you",
-    "Chicagoooo here we comeeeee !!!"
+    "Chicagoooo here we comeeeee !!!",
     "ur a lil shit !!!"
 ];
 
