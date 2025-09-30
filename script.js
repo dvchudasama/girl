1bf99724234a023d6f7b7f5d6ead7cf9679dbc3e
dvchudasama <hitidarshan@gmail.com>
2025-09-30 12:11:52 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,13 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2822
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue255;\red255\green255\blue255;\red45\green45\blue45;
-\red11\green90\blue180;\red0\green0\blue0;\red144\green1\blue18;\red101\green76\blue29;\red0\green0\blue109;
-}
-{\*\expandedcolortbl;;\cssrgb\c0\c0\c100000;\cssrgb\c100000\c100000\c100000;\cssrgb\c23137\c23137\c23137;
-\cssrgb\c0\c43922\c75686;\cssrgb\c0\c0\c0;\cssrgb\c63922\c8235\c8235;\cssrgb\c47451\c36863\c14902;\cssrgb\c0\c6275\c50196;
-}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
+const messages = [
+    "You been lookin pretty af lately.",
+    "My heart does a little dance every time I see your name pop up on my phone.",
+    "Every day I spend with you is a new favorite day. You make everything better.",
+    "Thanks for being my girl :)",
+    "Can't wait to do life w you",
+    "Chicagoooo here we comeeeee !!!"
+];
 
-\f0\fs24 \cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 const\cf4 \cb3 \strokec4  \cf5 \cb3 \strokec5 messages\cf4 \cb3 \strokec4  \cf4 \cb3 \strokec6 =\cf4 \cb3 \strokec4  [\cb1 \
-\pard\pardeftab720\partightenfactor0
-\cf4 \cb3     \cf7 \strokec7 "You been lookin pretty af lately."\cf4 \strokec4 ,\cb1 \
-\cb3     \cf7 \strokec7 "My heart does a little dance every time I see your name pop up on my phone."\cf4 \strokec4 ,\cb1 \
-\cb3     \cf7 \strokec7 "Every day I spend with you is a new favorite day. You make everything better."\cf4 \strokec4 ,\cb1 \
-\cb3     \cf7 \strokec7 "Thanks for being my girl :)"\cf4 \strokec4 ,\cb1 \
-\cb3     \cf7 \strokec7 "Can't wait to do life w you"\cf4 \strokec4 ,\cb1 \
-\cb3     \cf7 \strokec7 "Chicagoooo here we comeeeee !!!"\cf4 \cb1 \strokec4 \
-\cb3 ];\cb1 \
-\
-\pard\pardeftab720\partightenfactor0
-\cf2 \cb3 \strokec2 function\cf4 \cb3 \strokec4  \cf8 \strokec8 showMessage\cf4 \strokec4 (\cf9 \cb3 \strokec9 index\cf4 \cb3 \strokec4 ) \{\cb1 \
-\pard\pardeftab720\partightenfactor0
-\cf4 \cb3     \cf2 \cb3 \strokec2 const\cf4 \cb3 \strokec4  \cf5 \cb3 \strokec5 messageDisplay\cf4 \cb3 \strokec4  \cf4 \cb3 \strokec6 =\cf4 \cb3 \strokec4  \cf9 \cb3 \strokec9 document\cf4 \cb3 \strokec4 .\cf8 \strokec8 getElementById\cf4 \strokec4 (\cf7 \strokec7 'message-display'\cf4 \strokec4 );\cb1 \
-\cb3     \cf9 \cb3 \strokec9 messageDisplay\cf4 \cb3 \strokec4 .\cf9 \cb3 \strokec9 textContent\cf4 \cb3 \strokec4  \cf4 \cb3 \strokec6 =\cf4 \cb3 \strokec4  \cf9 \cb3 \strokec9 messages\cf4 \cb3 \strokec4 [\cf9 \cb3 \strokec9 index\cf4 \cb3 \strokec4 ];\cb1 \
-\cb3 \}\cb1 \
+function showMessage(index) {
+    const messageDisplay = document.getElementById('message-display');
+    messageDisplay.textContent = messages[index];
 }
